437eb8af6c99623c12a8738899009c4873061470
justinthomas22 <thomasequihen@gmail.com>
2025-06-10 16:06:26 +0200

    Suppression + Ajout
    Suppression des flèches ( annuler et rétablir) + Ajout des informations concernant le projet ( nom, auteur, ... )

--- Application1/exemple.py
+++ Application1/exemple.py
@@ -1,9 +1,8 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow, QToolBar, QStatusBar, \
-                            QLabel, QTextEdit, QFileDialog, QDockWidget
+                            QLabel, QTextEdit, QFileDialog, QDockWidget, QWidget, QLineEdit, QFormLayout, QDateEdit
 from PyQt6.QtGui import QIcon, QAction, QPixmap, QPainter
-from PyQt6.QtCore import Qt
-
+from PyQt6.QtCore import Qt, QDate
 
 # --- class widget: hérite de QLabel ------------------------------------------
 class Image(QLabel):
@@ -65,8 +64,6 @@ class Image(QLabel):
 
         print(f"Carré cliqué: ({grid_x}, {grid_y})")
 
-
-
 # -----------------------------------------------------------------------------
 # --- class FenetreAppli
 # -----------------------------------------------------------------------------
@@ -74,7 +71,7 @@ class FenetreAppli(QMainWindow):
     def __init__(self, chemin: str = None):
         super().__init__()
         self.__chemin = chemin
-        
+
         self.setWindowTitle("Plan_magasin")
         self.setWindowIcon(QIcon(sys.path[0] + '/icones/logo_but.png'))
         self.setGeometry(100, 100, 500, 300)
@@ -82,14 +79,31 @@ class FenetreAppli(QMainWindow):
         # widget central
 
         # dock
-        self.dock = QDockWidget('Bloc Notes')
-        self.addDockWidget(Qt.DockWidgetArea.LeftDockWidgetArea, self.dock)
-        self.dock.setMaximumWidth(400)
-        self.text_edit = QTextEdit()
-        self.dock.setWidget(self.text_edit)
+        self.dock = QDockWidget('Nouveau Projet', self)
+        self.dock.setMinimumWidth(20)
 
-        
-        
+        # Permet de s'afficher en même temps que le chargement de la carte du magasin
+        self.dock.setVisible(False) 
+        self.addDockWidget(Qt.DockWidgetArea.LeftDockWidgetArea, self.dock)
+
+        self.form_widget = QWidget()
+        self.form_layout = QFormLayout()
+
+        self.nom_projet_edit = QLineEdit()
+        self.auteur_edit = QLineEdit()
+        self.date_creation_edit = QDateEdit()
+        self.date_creation_edit.setDate(QDate.currentDate())
+        self.nom_magasin_edit = QLineEdit()
+        self.adresse_magasin_edit = QLineEdit()
+
+        self.form_layout.addRow('Nom du Projet :', self.nom_projet_edit)
+        self.form_layout.addRow('Auteur :', self.auteur_edit)
+        self.form_layout.addRow('Date de Création :', self.date_creation_edit)
+        self.form_layout.addRow('Nom du Magasin :', self.nom_magasin_edit)
+        self.form_layout.addRow('Adresse du Magasin :', self.adresse_magasin_edit)
+
+        self.form_widget.setLayout(self.form_layout)
+        self.dock.setWidget(self.form_widget)
 
         # barre d'état
         self.barre_etat = QStatusBar()
@@ -109,31 +123,16 @@ class FenetreAppli(QMainWindow):
         menu_fichier.addSeparator()
         menu_fichier.addAction('Quitter', self.destroy)
 
-        action_annuler = QAction(QIcon(sys.path[0] + '/icones/left.png'), 'Précédent', self)
-        action_annuler.setShortcut('Ctrl+Z')
-        action_annuler.triggered.connect(self.text_edit.undo)
-        menu_edition.addAction(action_annuler)
-
-        action_retablir = QAction(QIcon(sys.path[0] + '/icones/right.png'), 'Rétablir', self)
-        action_retablir.setShortcut('Ctrl+Y')
-        action_retablir.triggered.connect(self.text_edit.redo)
-        menu_edition.addAction(action_retablir)
-
         # ajout d'une barre d'outils
         barre_outil = QToolBar('Principaux outils')
         self.addToolBar(barre_outil)
-
-        barre_outil.addAction(action_annuler)
-        barre_outil.addAction(action_retablir)
 
         # Action pour afficher l'image
         action_afficher_image = QAction('Afficher le plan', self)
         action_afficher_image.triggered.connect(self.ouvrir_plan)
         barre_outil.addAction(action_afficher_image)
 
-
         self.showMaximized()
-
 
     def nouveau(self):
         self.barre_etat.showMessage('Créer un nouveau ....', 2000)
@@ -141,7 +140,6 @@ class FenetreAppli(QMainWindow):
         chemin, validation = boite.getOpenFileName(directory = sys.path[0])
         if validation:
             self.__chemin = chemin
-
 
     def ouvrir(self):
         self.barre_etat.showMessage('Ouvrir un nouveau....', 2000)
@@ -151,7 +149,6 @@ class FenetreAppli(QMainWindow):
             self.__chemin = chemin
             self.affiche_image(taille_cellule = 13)
 
-
     def enregistrer(self):
         self.barre_etat.showMessage('Enregistrer....', 2000 )
         boite = QFileDialog()
@@ -159,13 +156,11 @@ class FenetreAppli(QMainWindow):
         if validation:
             self.__chemin = chemin
 
-        
     def affiche_image(self, taille_cellule=10):
         self.image = Image(self.__chemin, taille_cellule=taille_cellule)  # Taille de cellule ajustable
         self.image.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.setCentralWidget(self.image)
-
-
+        self.dock.setVisible(True)  # Afficher le dock lorsque l'image est chargée
 
     def ouvrir_plan(self):
         """Permet de choisir parmi les fichiers un plan de magasin"""
@@ -179,12 +174,8 @@ class FenetreAppli(QMainWindow):
             self.__chemin = fichier
             self.affiche_image(taille_cellule = 10)
 
-
-
-
 # --- main --------------------------------------------------------------------
 if __name__ == "__main__":
-
     # création d'une QApplication
     app = QApplication(sys.argv)
 
@@ -197,7 +188,6 @@ if __name__ == "__main__":
     #     app.setStyleSheet(qss)
 
     fenetre = FenetreAppli()
-
 
     # lancement de l'application
     sys.exit(app.exec())
