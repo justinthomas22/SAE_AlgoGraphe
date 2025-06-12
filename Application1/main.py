911bcc424e9b072ded253ee3d1535049e5a3ed4e
Jordan SOW <nsow002@outlook.com>
2025-06-12 13:57:41 +0200

    Application 1 en MVC

---
+++ Application1/main.py
@@ -0,0 +1,12 @@
+import sys
+from PyQt6.QtWidgets import QApplication
+from controller import MagasinController
+
+def main():
+    app = QApplication(sys.argv)
+    controller = MagasinController()
+    fenetre = controller.run()
+    sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()
